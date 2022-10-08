cf63ace87cfbd86801bc5bf402db48a76522a46b
Ranielli <raniellimontagna@hotmail.com>
2022-10-08 15:03:57 -0300

    feat: adiciona validações e tratamento de erro

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,5 +1,7 @@
 import { Router } from "express";
 import { prismaClient } from "../database/prismaClient";
+import { isNumber } from "../utils/validations/isNumber";
+import { required } from "../utils/validations/required";
 
 const usersRoutes = Router();
 
@@ -17,6 +19,10 @@ usersRoutes.post("/create", async (req, res) => {
   // Recebendo os dados do usuário
   const user = req.body;
 
+  required(user.name, "name");
+  required(user.email, "email");
+  required(user.password, "password");
+
   // Salvando o usuário no banco de dados
   const createdUser = await prismaClient.user.create({
     data: {
@@ -31,18 +37,62 @@ usersRoutes.post("/create", async (req, res) => {
 });
 
 // Endpoint para editar um usuário
-usersRoutes.put("/update", (req, res) => {
-  res.send("Atualizando usuário");
+usersRoutes.put("/update", async (req, res) => {
+  // Recebendo os dados do usuário
+  const user = req.body;
+
+  required(user.id, "id");
+
+  // Atualizando o usuário no banco de dados
+  const editedUser = await prismaClient.user.update({
+    where: {
+      id: user.id,
+    },
+    data: {
+      name: user.name,
+      email: user.email,
+      password: user.password,
+    },
+  });
+
+  // Retornando o usuário editado
+  res.json(editedUser);
 });
 
 // Endpoint para deletar um usuário
-usersRoutes.delete("/delete", (req, res) => {
-  res.send("Deletando usuário");
+usersRoutes.delete("/delete", async (req, res) => {
+  // Recebendo os dados do usuário
+  const user = req.body;
+
+  required(user.id, "id");
+
+  // Deletando o usuário no banco de dados
+  await prismaClient.user.delete({
+    where: {
+      id: user.id,
+    },
+  });
+
+  res.send("Usuário deletado com sucesso!");
 });
 
 // Endpoint para buscar um usuário
-usersRoutes.get("/findById/:id", (req, res) => {
-  res.send("Buscando usuário");
+usersRoutes.get("/findById/:id", async (req, res) => {
+  // Recebendo o id do usuário
+  const params = req.params;
+
+  required(params.id, "id");
+  isNumber(params.id, "id");
+
+  // Buscando o usuário no banco de dados
+  const user = await prismaClient.user.findUnique({
+    where: {
+      id: Number(params.id),
+    },
+  });
+
+  // Retornando o usuário
+  res.json(user);
 });
 
 export default usersRoutes;
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,8 @@
-import express, { Request, Response } from "express";
+import express from "express";
+import "express-async-errors";
+
 import routes from "./routes/routes";
+import { ErrorMiddleware } from "./middlewares/error";
 
 const app = express();
 
@@ -9,6 +12,10 @@ app.use(express.json());
 //Rotas
 app.use(routes);
 
+//Tratar erros
+app.use(ErrorMiddleware);
+
+//Iniciar o servidor
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
